f65841f45a00f73c76d94aa909003a54af4ee91d
Andrej994x <andrej.nesti@gmail.com>
2026-07-09 17:55:33 +0200

    Add EF Core DbContext

---
+++ AbySalto.CartService/AbySalto.CartService/Data/CartDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AbySalto.CartService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbySalto.CartService.Data;
+
+public class CartDbContext : DbContext
+{
+    public CartDbContext(DbContextOptions<CartDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Cart> Carts => Set<Cart>();
+
+    public DbSet<CartItem> CartItems => Set<CartItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cart>()
+            .HasMany(c => c.Items)
+            .WithOne(i => i.Cart)
+            .HasForeignKey(i => i.CartId);
+
+        modelBuilder.Entity<CartItem>()
+            .Property(i => i.UnitPrice)
+            .HasPrecision(18, 2);
+    }
+}
