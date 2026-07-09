5967f3555a5f3fcb4efd7c4ee0c5e62b49990b5b
Andrej994x <andrej.nesti@gmail.com>
2026-07-09 18:23:22 +0200

    Add cart service interface

---
+++ AbySalto.CartService/AbySalto.CartService/Services/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using AbySalto.CartService.DTOs;
+
+namespace AbySalto.CartService.Services;
+
+public interface ICartService
+{
+    Task<CartResponse?> GetCartAsync(string userId);
+
+    Task<CartResponse> AddItemAsync(string userId, AddCartItemRequest request);
+
+    Task<bool> UpdateItemQuantityAsync(string userId, Guid itemId, UpdateCartItemRequest request);
+
+    Task<bool> RemoveItemAsync(string userId, Guid itemId);
+
+    Task<bool> ClearCartAsync(string userId);
+}
