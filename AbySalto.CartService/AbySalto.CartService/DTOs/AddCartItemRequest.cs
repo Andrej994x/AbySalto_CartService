136982ebaf30cf6e4856cc7a2ad48e766f488be0
Andrej994x <andrej.nesti@gmail.com>
2026-07-09 17:53:37 +0200

    Add cart request and response DTOs

---
+++ AbySalto.CartService/AbySalto.CartService/DTOs/AddCartItemRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbySalto.CartService.DTOs;
+
+public class AddCartItemRequest
+{
+    [Required]
+    public Guid ProductId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string ProductName { get; set; } = string.Empty;
+
+    [Range(1, 999)]
+    public int Quantity { get; set; }
+
+    [Range(0.01, 999999)]
+    public decimal UnitPrice { get; set; }
+}
---
+++ AbySalto.CartService/AbySalto.CartService/DTOs/CartResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace AbySalto.CartService.DTOs;
+
+public class CartResponse
+{
+    public Guid CartId { get; set; }
+
+    public string UserId { get; set; } = string.Empty;
+
+    public List<CartItemResponse> Items { get; set; } = new();
+
+    public decimal TotalAmount { get; set; }
+}
