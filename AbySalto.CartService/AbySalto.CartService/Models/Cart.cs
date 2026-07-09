1ae7bd4f7058c5f5972dc3a7e64ef73505265893
Andrej994x <andrej.nesti@gmail.com>
2026-07-09 17:51:47 +0200

    Add cart domain models

---
+++ AbySalto.CartService/AbySalto.CartService/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿namespace AbySalto.CartService.Models;
+
+public class Cart
+{
+    public Guid Id { get; set; }
+
+    public string UserId { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
+}
